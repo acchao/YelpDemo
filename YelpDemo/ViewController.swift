5ad9fda2a7a7221c1d4f2ad76ce05dbdeb7a05b6
Andrew Chao <andrew@Andrews-MacBook-Pro-2.local>
2014-09-23 20:49:53 -0700

    initial commit
    includes some autolayout
    filterview with filters, but not hooked into yelp's api
    search bar

--- YelpDemo/ViewController.swift
+++ YelpDemo/ViewController.swift
@@ -8,16 +8,102 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, UISearchDisplayDelegate, FilterViewControllerDelegate{
+
+    var client: YelpClient!
+
+    let yelpConsumerKey = "kJpw8S6LTK2wrVRxdb70NQ"
+    let yelpConsumerSecret = "Di5O_j3uiaqBqwNd_n6hN-tEN1Q"
+    let yelpToken = "T7P36RE-OFFw3enHKIKXVo8dg7wV40m2"
+    let yelpTokenSecret = "vcaRuIfMeiWC7VTKCJgd7fT5foM"
+
+    @IBOutlet weak var yelpTableView: UITableView!
+    @IBOutlet weak var navigationTitle: UINavigationItem!
+    var searchBar: UISearchBar!
+
+    var restaurants:[Restaurant] = []
+    var searchTerm: String!
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        yelpTableView.delegate = self
+        yelpTableView.dataSource = self
+
+        client = YelpClient(consumerKey: yelpConsumerKey, consumerSecret: yelpConsumerSecret, accessToken: yelpToken, accessSecret: yelpTokenSecret)
+
+        searchBar = UISearchBar()
+        searchBar.delegate = self
+        navigationItem.titleView = self.searchBar
+
+        self.searchTerm = "Thai"
+        reloadTableView()
     }
+
+    // Reloads the Table View with Box Office Movies
+    func reloadTableView()
+    {
+        client.searchWithTerm(self.searchTerm, success: { (operation: AFHTTPRequestOperation!, response: AnyObject!) -> Void in
+            var restaurantDictionaries = (response as NSDictionary)["businesses"] as [NSDictionary]
+
+            self.restaurants = restaurantDictionaries.map({ (business: NSDictionary) -> Restaurant in
+                Restaurant(dictionary: business)
+            })
+
+            self.yelpTableView.reloadData()
+            }) { (operation: AFHTTPRequestOperation!, error: NSError!) -> Void in
+                println(error)
+        }
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurants.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
+        tableView.rowHeight = UITableViewAutomaticDimension
+
+        var cell = tableView.dequeueReusableCellWithIdentifier("YelpCell") as YelpCell
+
+        cell.restaurant = self.restaurants[indexPath.row]
+        return cell
+
+    }
+
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: false)
+    }
+
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        self.searchTerm = searchBar.text
+        self.reloadTableView()
+    }
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        var filtersNavigationViewController = segue.destinationViewController as UINavigationController
+
+        var filterViewController = filtersNavigationViewController.viewControllers[0] as FilterViewController
+        filterViewController.delegate = self
+    }
+
+    func filterViewControllerSearchButtonClicked(filterViewController: FilterViewController) {
+        self.reloadTableView()
     }
 
 
